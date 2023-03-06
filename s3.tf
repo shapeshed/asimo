1bd820a4a56ba91384054d93ac842419b85142bd
George Ornbo <george@shapeshed.com>
2023-03-06 11:43:26 +0000

    Add more s3 buckets (#22)

--- s3.tf
+++ s3.tf
@@ -1,39 +1,14 @@
-#################################
-# creative-corners
-#################################
-resource "aws_s3_bucket" "creative-corners" {
-  bucket = "creative-corners"
-  tags = {
-    type = "client"
-  }
-}
-
-resource "aws_s3_bucket_acl" "creative-corners_acl" {
-  bucket = aws_s3_bucket.creative-corners.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "creative-corners_versioning" {
-  bucket = aws_s3_bucket.creative-corners.id
-  versioning_configuration {
-    status = "Suspended"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "creative-corners_public_access_block" {
-  bucket                  = aws_s3_bucket.creative-corners.id
+module "s3-creative-corners" {
+  source                  = "terraform-aws-modules/s3-bucket/aws"
+  version                 = "3.7.0"
+  bucket                  = "creative-corners"
+  acl                     = "private"
   block_public_acls       = true
-  block_public_policy     = true
   ignore_public_acls      = true
+  block_public_policy     = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "creative-corners_bucket_encryption" {
-  bucket = aws_s3_bucket.creative-corners.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
+  versioning = {
+    enabled = false
   }
 }
 module "s3-photos-shapeshed-com" {
@@ -49,4 +24,11 @@ module "s3-photos-shapeshed-com" {
     enabled = false
   }
 }
-
+module "s3-static-shapeshed-com" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+  bucket  = "static.shapeshed.com"
+  versioning = {
+    enabled = false
+  }
+}
