569553f56e0b67645ee9c7d823a665776dc06973
George Ornbo <george@shapeshed.com>
2023-03-06 12:09:44 +0000

    Add bomberg

--- s3.tf
+++ s3.tf
@@ -32,3 +32,16 @@ module "s3-static-shapeshed-com" {
     enabled = false
   }
 }
+module "bomberg" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+  providers = {
+    aws = aws.us-east-1
+  }
+  bucket                  = "bomberg"
+  acl                     = "private"
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
