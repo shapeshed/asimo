22a133edafbdd36d83f30b72258c41df6fbe54db
George Ornbo <george@shapeshed.com>
2023-03-15 09:15:58 +0000

    Add acm validation (#59)

--- certificate.tf
+++ certificate.tf
@@ -20,3 +20,26 @@ resource "aws_acm_certificate_validation" "cert-shapeshed-com-validation" {
   certificate_arn         = aws_acm_certificate.cert-shapeshed-com.arn
   validation_record_fqdns = [for record in aws_route53_record.shapeshed_com_acm_validation : record.fqdn]
 }
+
+resource "aws_acm_certificate" "samornbo-com" {
+  provider      = aws.us-east-1
+  domain_name   = "samornbo.com"
+  key_algorithm = "RSA_2048"
+  subject_alternative_names = [
+    "*.samornbo.com",
+    "samornbo.com",
+  ]
+  tags              = {}
+  tags_all          = {}
+  validation_method = "DNS"
+
+  options {
+    certificate_transparency_logging_preference = "ENABLED"
+  }
+}
+
+resource "aws_acm_certificate_validation" "samornbo-com" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.samornbo-com.arn
+  validation_record_fqdns = [for record in aws_route53_record.samornbo_com_acm_validation : record.fqdn]
+}
--- route53.tf
+++ route53.tf
@@ -198,3 +198,24 @@ resource "aws_route53_record" "__dmarc_shapeshed_com_TXT" {
   ]
   ttl = 60
 }
+
+resource "aws_route53_zone" "samornbo-com" {
+  name = "samornbo.com"
+}
+
+resource "aws_route53_record" "samornbo_com_acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.samornbo-com.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.samornbo-com.zone_id
+}
