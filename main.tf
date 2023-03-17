bf3b5a06d066cb5ea9bb14b41f87978878e76e12
George Ornbo <george@shapeshed.com>
2023-03-17 10:59:28 +0000

    Move require_version

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "~> 1.4.0"
   cloud {
     organization = "shapeshed"
 
@@ -12,8 +13,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-  required_version = "~> 1.4.0"
-
 }
 
 provider "aws" {
