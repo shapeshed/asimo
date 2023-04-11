2c65827666cb3c09b1a1223520e4a7fe43c75a96
George Ornbo <george@shapeshed.com>
2023-04-11 17:21:32 +0100

    Add ARN

--- eventbridge.tf
+++ eventbridge.tf
@@ -2,4 +2,7 @@ resource "aws_scheduler_schedule" "daily_3am" {
   name                = "daily_3am"
   group_name          = "default"
   schedule_expression = "cron(0 3 * * ? *)"
+  target {
+    arn = "arn:aws:lambda:eu-west-2:535487841971:function:test"
+  }
 }
